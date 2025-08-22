203e722e156ac5611e2b8fcd3a4df03aa646119e
Rajat22UEE <rajatnath6909@gmail.com>
2025-08-22 09:38:51 +0530

    ltst

--- admin/next.config.mjs
+++ admin/next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true
+    reactStrictMode: true,
+    experimental: {
+    optimizeCss: false, // disable lightningcss, fallback to postcss
+  },
 };
 
 export default nextConfig;
