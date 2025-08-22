a2bce1fa812c2f9431b1b0218ed8e09fe061d584
Rajat22UEE <rajatnath6909@gmail.com>
2025-08-22 09:46:49 +0530

    ncfg

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    experimental: {
+    optimizeCss: false, // disable lightningcss, fallback to postcss
+  },
+};
 
 export default nextConfig;
